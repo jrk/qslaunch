d1ad87ddc776f03a089027fe7394be1b761de898
serge <none@none>
2010-06-24 20:49:31 -0400

    Reorg of setup.py

--- setup.py
+++ setup.py
@@ -7,87 +7,36 @@ setup.py
 :license: Apache License 2.0
 """
 
-from distutils.core import setup
-from distutils.command.install_data import install_data
-from distutils.command.install import INSTALL_SCHEMES
-import os
-import sys
+__version__ = '0.3.0'
 
-class osx_install_data(install_data):
-    # On MacOS, the platform-specific lib dir is /System/Library/Framework/Python/.../
-    # which is wrong. Python 2.5 supplied with MacOS 10.5 has an Apple-specific fix
-    # for this in distutils.command.install_data#306. It fixes install_lib but not
-    # install_data, which is why we roll our own install_data class.
-    
-    def finalize_options(self):
-        # By the time finalize_options is called, install.install_lib is set to the
-        # fixed directory, so we set the installdir to install_lib. The
-        # install_data class uses ('install_data', 'install_dir') instead.
-        self.set_undefined_options('install', ('install_lib', 'install_dir'))
-        install_data.finalize_options(self)
+from setuptools import setup, find_packages
 
-
-
-if sys.platform == "darwin":
-    cmdclasses = {'install_data': osx_install_data}
-else:
-    cmdclasses = {'install_data': install_data}
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-# Tell distutils to put the data_files in platform-specific installation
-# locations. See here for an explanation:
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-packages, data_files = [], []
-walk_paths = ['qslaunch']
-for walk_path in walk_paths:
-    for dirpath, dirnames, filenames in os.walk(walk_path):
-        # Ignore dirnames that start with '.'
-        for i, dirname in enumerate(dirnames):
-            if dirname.startswith('.'): del dirnames[i]
-        if '__init__.py' in filenames:
-            packages.append('.'.join(fullsplit(dirpath)))
-        elif filenames:
-            data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
-
-scripts = []
-for dirpath, dirnames, filenames in os.walk('bin'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    scripts += [os.path.join(dirpath, f) for f in filenames]
-
-
-# Dynamically calculate the version based on agifm.VERSION.
-version = __import__('qslaunch').get_version()
-if u'HG' in version:
-    version = ' '.join(version.split(' ')[:-1])
-
-setup(name='qslaunch',
-    version= version.replace(' ', '-'),
+setup(
+    name='qslaunch',
+    version= __version__,
     description='Simple QuickSilver launcher helper',
-    author='Serge Emond',
+    long_description=u"""\
+``qslaunch`` is a small framework to facilitate launching shell stuff from Quicksilver_.
+
+The latest version is available in a `Mercurial repository`_.
+
+.. _Mercurial repository: http://bitbucket.org/greyw/qslaunch/
+.. _Quicksilver: http://www.blacktree.com/
+""",
+    author=u'Serge Émond',
     author_email='greyl@greyworld.net',
-    license='Apache License 2.0',
     url='http://greyworld.net/en/projects/qslaunch/',
-    packages=packages,
-    cmdclass=cmdclasses,
-    data_files=data_files,
-    scripts=scripts,
+    license='Apache License 2.0',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: MacOS X',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
     install_requires=["appscript"],
+    packages=find_packages(),
+    zip_safe=False,
     )
